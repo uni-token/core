e13dd64b0702b224e29731f623aa043d6ecfb9c4
_Kerman <kermanx@qq.com>
2025-08-21 14:24:14 +0800

    chore: release v0.0.2

--- bump.config.ts
+++ bump.config.ts
@@ -2,11 +2,12 @@ import { readFileSync, writeFileSync } from 'node:fs'
 import { resolve } from 'node:path'
 import { parse, stringify } from '@std/toml'
 import { defineConfig } from 'bumpp'
+import { $ } from 'zx'
 
 export default defineConfig({
   files: ['sdk/*/package.json'],
   all: true,
-  execute(config) {
+  async execute(config) {
     const projectToml = resolve(import.meta.dirname, './sdk/python/pyproject.toml')
     const projectConfig = parse(readFileSync(projectToml, 'utf-8')) as any
     const version = config.results.newVersion
@@ -14,5 +15,6 @@ export default defineConfig({
     writeFileSync(projectToml, stringify(projectConfig, {
       keyAlignment: true,
     }), 'utf-8')
+    await $`pnpm lint --fix`
   },
 })
